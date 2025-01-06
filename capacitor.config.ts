0043a2ead0283872234f622a6b5c0753b9be3cd8
israeloriente <israelnunesoriente@gmail.com>
2025-01-06 01:06:48 +0000

    build: ionic capacitor in live dev

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.orientech.miicare',
-  appName: 'miicare',
-  webDir: 'www',
-};
-
-export default config;
